b5e1a7149da0219991be8ecc24a5d4c7f056c8d4
Smruti <lenkasmruti123@gmail.com>
2021-10-14 12:23:26 +0530

    Added: Uc3_MaximumString

--- MaximumNumber/MaximumOfThree.java
+++ MaximumNumber/MaximumOfThree.java
@@ -2,8 +2,8 @@ package MaximumNumber;
 
 public class MaximumOfThree {
 		
-		public static Float maxOfThree(Float x, Float y,Float z) {
-			Float max = x;
+		public static String maxOfThree(String x, String y, String z) {
+			String max = x;
 			if (y.compareTo(max) > 0)
 				max = y;
 			if (z.compareTo(max) > 0)
@@ -12,7 +12,7 @@ public class MaximumOfThree {
 		}
 
 		public static void main(String[] args) {
-			Float maximum = MaximumOfThree.maxOfThree(34.0f, 36.5f, 38.8f);
+			String maximum = MaximumOfThree.maxOfThree("Apple", "peach", "Banana");
 			System.out.println("Maximum  three numbers is :" + maximum );
 		}
 }
