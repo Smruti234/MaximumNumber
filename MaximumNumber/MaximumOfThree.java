98407c384722db6c9b0ae72dc99f4f12a6fd0c95
Smruti <lenkasmruti123@gmail.com>
2021-10-14 21:36:08 +0530

    Added: Refactor1

--- MaximumNumber/MaximumOfThree.java
+++ MaximumNumber/MaximumOfThree.java
@@ -1,22 +1,23 @@
 package MaximumNumber;
 
 public class MaximumOfThree {
-		
-		public static String maxOfThree(String x, String y, String z) {
-			String max = x;
-			if (y.compareTo(max) > 0)
-				max = y;
-			if (z.compareTo(max) > 0)
-				max = z;
-			return max;
-		}
 
-		public static void main(String[] args) {
-			String maximum = MaximumOfThree.maxOfThree("Apple", "peach", "Banana");
-			System.out.println("Maximum  three numbers is :" + maximum );
-		}
+	public static <E extends Comparable<E>> E maximum(E x, E y, E z) {
+		E max = x;
+
+		if (y.compareTo(max) > 0)
+			max = y;
+
+		if (z.compareTo(max) > 0)
+			max = z;
+
+		return max;
+	}
+
+	public static void main(String args[]) {
+		System.out.printf("Maximum of %d, %d and %d is %d\n\n", 5, 7, 6, maximum(5, 7, 6));
+		System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.4, 8.9, 7.0, maximum(6.4, 8.9, 7.0));
+		System.out.printf("Maximum of %s, %s and %s is %s\n", "banana", "apple", "peach",
+				maximum("banana", "apple", "peach"));
+	}
 }
-	
-	
-
-
