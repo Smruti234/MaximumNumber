3550a2c657129cb08acf1a8910183044255430f7
Smruti <lenkasmruti123@gmail.com>
2021-10-17 17:44:18 +0530

    Added: Uc5_MaxMethod

--- MaximumNumber/MaximumTest.java
+++ MaximumNumber/MaximumTest.java
@@ -1,31 +1,39 @@
 package MaximumNumber;
-public class MaximumTest<E extends Comparable<E>> {
-	E[] value;
 
-	public MaximumTest(E[] value) {
-		this.value = value;
+public class MaximumTest<A extends Comparable<A>> {
+	A[] multiple;
+
+	public MaximumTest(A[] multiple) {
+		this.multiple = multiple;
 	}
 
-	public E maximum() {
-		return MaximumTest.maximum(value);
+	public A maximum() {
+		return MaximumTest.maximum(multiple);
 	}
 
-	public static <E extends Comparable<E>> E maximum(E[] value) {
-		for (int i = 0; i < value.length; i++) {
-			for (int K = i; K < value.length; K++) {
-				if (value[i].compareTo(value[K]) < 0) {
-					value[i] = value[K];
+	public static <A extends Comparable<A>> A maximum(A[] multiple) {
+		for (int i = 0; i < multiple.length; i++) {
+			for (int p = i; p < multiple.length; p++) {
+				if (multiple[i].compareTo(multiple[p]) < 0) {
+					A x = multiple[i];
+					multiple[i] = multiple[p];
+					multiple[p] = x;
 				}
 			}
 		}
-		return value[0];
+		MaximumTest.printMax(multiple[0]);
+		return multiple[0];
+	}
+
+	private static <A> void printMax(A maximum) {
+		System.out.println("Maximum number is :" + " " + maximum);
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
-		String[] multiString = { "Ant","Catfish","Elephant","molly" };
-		System.out.println("The Maximum value is :" + " " + new MaximumTest<String>(multiString).maximum());
+		Integer[] multiInteger = { 77, 83, 98, 23, 56 };
+		System.out.println("The Maximum value is :" + " " + new MaximumTest<Integer>(multiInteger).maximum());
 
 	}
 
 }
- 
